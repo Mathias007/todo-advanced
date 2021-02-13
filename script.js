ab98ef4487b4752317b5c56b2cd47e68281b3dd7
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-13 19:28:50 +0100

    handle editing and deleting task + closing popup

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ const prepareDOMEvents = () => {
     $addBtn.addEventListener("click", addNewTask);
     $todoInput.addEventListener("keyup", enterCheck);
     $ulList.addEventListener("click", checkClick);
+    $addPopupBtn.addEventListener("click", changeTodo);
+    $closeTodoBtn.addEventListener("click", closePopup);
 };
 
 const addNewTask = () => {
@@ -87,11 +89,43 @@ const checkClick = (e) => {
             e.target.closest("li").classList.toggle("completed");
             e.target.closest("button").classList.toggle("completed");
         } else if (e.target.closest("button").classList.contains("edit")) {
-            console.log("edit");
+            editTask(e);
         } else if (e.target.closest("button").classList.contains("delete")) {
-            console.log("delete");
+            deleteTask(e);
         }
     }
+};
+
+const editTask = (e) => {
+    const oldTodo = e.target.closest("li").id;
+    $editedTodo = document.getElementById(oldTodo);
+    $popupInput.value = $editedTodo.firstChild.textContent;
+
+    $popup.style.display = "flex";
+};
+
+const changeTodo = () => {
+    if ($popupInput.value !== "") {
+        $editedTodo.firstChild.textContent = $popupInput.value;
+        $popup.style.display = "none";
+        $popupInfo.innerText = "";
+    } else {
+        $popupInfo.innerText = "Musisz podać jakąś treść!";
+    }
+};
+
+const deleteTask = (e) => {
+    const deleteTodo = e.target.closest("li");
+    deleteTodo.remove();
+
+    if ($allTasks.length === 0) {
+        $alertInfo.innerText = "Brak zadań na liście.";
+    }
+};
+
+const closePopup = () => {
+    $popup.style.display = "none";
+    $popupInfo.innerText = "";
 };
 
 document.addEventListener("DOMContentLoaded", main);
